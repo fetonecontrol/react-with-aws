c610e6b421067897f10f461c0923cbc0f8c4ec81
Frederick Ernest <frederick.ernest@gmail.com>
2020-10-22 15:28:02 -0700

    change graph colors

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,40 +1,76 @@
 import React, { PureComponent } from 'react';
 import {
-  ScatterChart, Scatter, XAxis, YAxis, ZAxis, CartesianGrid, Tooltip, Legend,
+  AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
 } from 'recharts';
 
-const data01 = [
-  { x: 100, y: 200, z: 200 }, { x: 120, y: 100, z: 260 },
-  { x: 170, y: 300, z: 400 }, { x: 140, y: 250, z: 280 },
-  { x: 150, y: 400, z: 500 }, { x: 110, y: 280, z: 200 },
-];
-const data02 = [
-  { x: 200, y: 260, z: 240 }, { x: 240, y: 290, z: 220 },
-  { x: 190, y: 290, z: 250 }, { x: 198, y: 250, z: 210 },
-  { x: 180, y: 280, z: 260 }, { x: 210, y: 220, z: 230 },
-];
-
+const data = [
+  {
+    "name": "Page A",
+    "uv": 4000,
+    "pv": 2400,
+    "amt": 2400
+  },
+  {
+    "name": "Page B",
+    "uv": 3000,
+    "pv": 1398,
+    "amt": 2210
+  },
+  {
+    "name": "Page C",
+    "uv": 2000,
+    "pv": 9800,
+    "amt": 2290
+  },
+  {
+    "name": "Page D",
+    "uv": 2780,
+    "pv": 3908,
+    "amt": 2000
+  },
+  {
+    "name": "Page E",
+    "uv": 1890,
+    "pv": 4800,
+    "amt": 2181
+  },
+  {
+    "name": "Page F",
+    "uv": 2390,
+    "pv": 3800,
+    "amt": 2500
+  },
+  {
+    "name": "Page G",
+    "uv": 3490,
+    "pv": 4300,
+    "amt": 2100
+  }
+]
+  
 export default class Example extends PureComponent {
-  static jsfiddleUrl = 'https://jsfiddle.net/alidingling/3mw50Lc9/';
 
   render() {
     return (
-      <ScatterChart
-        width={400}
-        height={400}
-        margin={{
-          top: 20, right: 20, bottom: 20, left: 20,
-        }}
-      >
-        <CartesianGrid />
-        <XAxis type="number" dataKey="x" name="stature" unit="cm" />
-        <YAxis type="number" dataKey="y" name="weight" unit="kg" />
-        <ZAxis type="number" dataKey="z" range={[60, 400]} name="score" unit="km" />
-        <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-        <Legend />
-        <Scatter name="A school" data={data01} fill="#8884d8" shape="star" />
-        <Scatter name="B school" data={data02} fill="#82ca9d" shape="triangle" />
-      </ScatterChart>
+      <AreaChart width={730} height={250} data={data}
+        margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+        <defs>
+          <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="5%" stopColor="#F66666" stopOpacity={0.8}/>
+            <stop offset="95%" stopColor="#FFF111" stopOpacity={0}/>
+          </linearGradient>
+          <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="5%" stopColor="#199999" stopOpacity={0.8}/>
+            <stop offset="95%" stopColor="#2EEEEE" stopOpacity={0}/>
+          </linearGradient>
+        </defs>
+        <XAxis dataKey="name" />
+        <YAxis />
+        <CartesianGrid strokeDasharray="3 3" />
+        <Tooltip />
+        <Area type="monotone" dataKey="uv" stroke="#666668" fillOpacity={1} fill="url(#colorUv)" />
+        <Area type="monotone" dataKey="pv" stroke="666666" fillOpacity={1} fill="url(#colorPv)" />
+      </AreaChart>  
     );
   }
 }
